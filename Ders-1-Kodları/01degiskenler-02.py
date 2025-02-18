f71c927cb66258d885c60921a402006c77e05b40
Dogukan Payal <101371046+dogukanpayal@users.noreply.github.com>
2025-02-19 02:59:57 +0300

    gun-1

---
+++ Ders-1-Kodları/01degiskenler-02.py
@@ -0,0 +1,12 @@
+benimdegiskenim=10
+benimdigerdegiskenim=20
+
+toplam=benimdegiskenim+benimdigerdegiskenim
+print(toplam)
+print(type(toplam))
+
+bolum=benimdegiskenim/benimdigerdegiskenim
+print(bolum)
+print(type(bolum))
+
+
